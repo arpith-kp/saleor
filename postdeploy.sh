35cc1145efbc61dab38817f87603b52c0b232ef8
Arpith Prakash <arpikp@gmail.com>
2018-01-25 19:02:56 +0530

    Add postdeploy script

---
+++ postdeploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser(email='admin@example.com', password='Password1')" | python manage.py shell
