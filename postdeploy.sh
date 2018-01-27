96299e9c91b3d088d93e1cfa0f69693ee19fc6e1
Arpith Prakash <arpikp@gmail.com>
2018-01-27 08:42:53 +0530

    Populate DB with sample data

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python manage.py migrate
+python manage.py populatedb
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser(email='admin@example.com', password='Password1')" | python manage.py shell
